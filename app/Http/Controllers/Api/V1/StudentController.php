45a2da30f43be93cc1d1407e484a86139a14f0d2
MaxKuftinec <116775534+MaxKuftinec@users.noreply.github.com>
2022-10-31 10:20:28 +0800

    Add files via upload

---
+++ app/Http/Controllers/Api/V1/StudentController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreStudentRequest;
+use App\Http\Requests\UpdateStudentRequest;
+use App\Models\Student;
+use App\Http\Resources\V1\StudentResource;
+use App\Http\Resources\V1\StudentCollection;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $students = Student::query();
+        if (request('name')) {
+            $students->where('name', 'Like', '%' . request('name') . '%');
+
+            return new StudentCollection($students->orderBy('id', 'DESC')->paginate());
+        }
+        if (request('email')) {
+            $students->where('email', 'Like', '%' . request('email') . '%');
+
+            return new StudentCollection($students->orderBy('id', 'DESC')->paginate());
+        }
+
+
+
+
+        return new StudentCollection(Student::paginate());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreStudentRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreStudentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Student $student)
+    {
+        return new StudentResource($student);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateStudentRequest  $request
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateStudentRequest $request, Student $student)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Student $student)
+    {
+        //
+    }
+}
